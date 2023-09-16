84c839847824fb9a6b094eb4c3572abd17560f88
Austin Hunt <austinsketches@gmail.com>
2023-09-16 15:41:31 -0400

    add logging

--- wp-content/plugins/user-meta-conditional-acf-form/user-meta-conditional-acf-form.php
+++ wp-content/plugins/user-meta-conditional-acf-form/user-meta-conditional-acf-form.php
@@ -100,8 +100,9 @@ if (!class_exists('UserMetaConditionalACFFormDisplay')) {
                 ),
                 $atts
             );
+            var_dump($attributes);
             if (empty($attributes['required_user_meta_properties']) or empty($attributes['acf_field_group_ids'])) {
-                $output = '<p>Please provide (at minimum) acf_field_group_ids="fg1,fg2,..." and required_user_meta_propertiess="propA,propB,propC,..." argument to the [user_meta_conditional_acf_display...] shortcode.</p>';
+                $output = '<p>Please provide (at minimum) acf_field_group_ids="fg1,fg2,..." and required_user_meta_properties="propA,propB,propC,..." argument to the [user_meta_conditional_acf_display...] shortcode.</p>';
                 return $output;
             }
 
