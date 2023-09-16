37852bf526660c8349c5467646d39a5bcea4f4a8
Austin Hunt <austinsketches@gmail.com>
2023-09-16 17:17:28 -0400

    add return arg to acf_form

--- wp-content/plugins/user-meta-conditional-acf-form/user-meta-conditional-acf-form.php
+++ wp-content/plugins/user-meta-conditional-acf-form/user-meta-conditional-acf-form.php
@@ -158,10 +158,12 @@ if (!class_exists('UserMetaConditionalACFFormDisplay')) {
                 }
                 $acf_field_group_ids = preg_replace('/\s*,\s*/', ',', filter_var($attributes['acf_field_group_ids']));
                 $acf_field_group_ids_array = explode(',', $acf_field_group_ids);
+                var_dump($redirect);
                 acf_form(array(
                     'post_id' => 'user_' . get_current_user_id(),
                     'id' => 'acf-form-' . $this->generateRandomString(),
                     'field_groups' => $acf_field_group_ids_array,
+                    'return' => $redirect
                 ));
             } else {
                 if (!empty($redirect)) {
