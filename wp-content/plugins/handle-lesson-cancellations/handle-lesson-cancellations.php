cca9cddb0cb6e942f128bde64281520615c82872
Austin Hunt <huntaj@cofc.edu>
2023-10-26 14:00:51 -0400

    increment makeup lesson credits using $bookings and $reservation

--- wp-content/plugins/handle-lesson-cancellations/handle-lesson-cancellations.php
+++ wp-content/plugins/handle-lesson-cancellations/handle-lesson-cancellations.php
@@ -27,13 +27,75 @@ if (!class_exists('HandleLessonCancellation')) {
             plugin_basename(__FILE__);
         }
 
+        function increment_virtual_makeup_lesson_credits($user_id)
+        {
+            error_log('increment_virtual_makeup_lesson_credits for user id: ' . $user_id . '');
+            // Get current values
+            $virtual_credits = get_field('virtual_makeup_lesson_credits', 'user_' . $user_id);
+            error_log('before value: ' . $virtual_credits . '');
+            // Increment values
+            $virtual_credits++;
+            error_log('after value: ' . $virtual_credits . '');
+            // Update fields with incremented values
+            update_field('virtual_makeup_lesson_credits', $virtual_credits, 'user_' . $user_id);
+        }
+        function increment_inperson_makeup_lesson_credits($user_id)
+        {
+            error_log('increment_inperson_makeup_lesson_credits for user id: ' . $user_id . '');
+            // Get current values 
+            $inperson_credits = get_field('inperson_makeup_lesson_credits', 'user_' . $user_id);
+
+            error_log('before value: ' . $inperson_credits . '');
+            // Increment values 
+            $inperson_credits++;
+
+            error_log('after value: ' . $inperson_credits . '');
+
+            // Update fields with incremented values 
+            update_field('inperson_makeup_lesson_credits', $inperson_credits, 'user_' . $user_id);
+        }
+
+        function get_amelia_service_category_name_from_service_id($service_id)
+        {
+            global $wpdb;
+            $category_id = $wpdb->get_var(
+                $wpdb->prepare(
+                    "SELECT categoryId FROM wp_amelia_services WHERE id = %d",
+                    $service_id
+                )
+            );
+
+            $category_name = $wpdb->get_var(
+                $wpdb->prepare(
+                    "SELECT name FROM wp_amelia_categories WHERE id = %d",
+                    $category_id
+                )
+            );
+            return $category_name;
+        }
+
         function handle_lesson_cancellation($reservation, $bookings, $container)
         {
-            // actions
-            error_log('HANDLE LESSON CANCELLATION');
-            error_log(print_r($reservation, true));
-            error_log(print_r($bookings, true));
-            error_log(print_r($container, true));
+
+            // Extract email from the array
+            $customer_email = $bookings[0]['customer']['email'];
+            error_log('customer email: ' . $customer_email . '');
+
+            // Get WordPress user by email
+            $user = get_user_by('email', $customer_email);
+
+            // Check if a user exists with the provided email
+            if ($user) {
+                echo 'User ID: ' . $user->ID;
+                $service_id = $reservation['serviceId'];
+                $category_name = $this->get_amelia_service_category_name_from_service_id($service_id);
+
+                if ($category_name == "Virtual Lessons") {
+                    $this->increment_virtual_makeup_lesson_credits($user->ID);
+                } else if ($category_name == "In Person Lessons") {
+                    $this->increment_inperson_makeup_lesson_credits($user->ID);
+                }
+            }
         }
 
 
