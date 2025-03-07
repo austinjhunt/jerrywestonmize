d78a85b5e70ed8c568896386347c45560bd3cb53
Austin Hunt <huntaj@cofc.edu>
2025-03-06 19:45:24 -0500

    add error logging

--- wp-content/plugins/show-only-for-regulars/show-only-for-regulars.php
+++ wp-content/plugins/show-only-for-regulars/show-only-for-regulars.php
@@ -60,6 +60,7 @@ if (!class_exists('ShowOnlyForRegulars')) {
         {
 
             ob_start();
+            error_log('show_only_for_regulars_callback');
             $attributes = shortcode_atts(
                 array(
                     'newcomer_redirect_uri' => '',
@@ -68,12 +69,16 @@ if (!class_exists('ShowOnlyForRegulars')) {
                 ),
                 $atts
             );
+
+            error_log('attributes:');
+            error_log($attributes);
             $login_url = "https://jerrywestonmize.com/index.php/login/";
             $current_url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
             $current_url = urlencode($current_url);
             $login_url = $login_url . '?redirect_to=' . $current_url . '';
             // redirect unauthenticated users to home page
             if (!is_user_logged_in()) {
+                error_log('user is not logged in');
                 echo "<script>window.location = '" . $login_url . "';</script>";
             }
 
@@ -82,26 +87,38 @@ if (!class_exists('ShowOnlyForRegulars')) {
 
             $customer_status = get_user_meta(user_id: get_current_user_id(), key: 'customer_status', single: true);
             $customer_status = explode(':', $customer_status)[0];
+
+            error_log('User ID: ' . get_current_user_id());
+            error_log('Customer status of user: ' . $customer_status);
             $customer_status = trim($customer_status);
             if ($customer_status == "regular") {
+                error_log('A');
                 if ($attributes['content_is_shortcode'] == true) {
+                    error_log('A1');
                     echo do_shortcode($content);
                 } else if ($attributes['content_is_shortcode'] == false) {
+                    error_log('A2');
                     echo $content;
                 }
             } else if ($customer_status == "newcomer") {
+                error_log('B');
                 if (!empty($attributes['newcomer_redirect_uri'])) {
+                    error_log('B1');
                     $redirect = $attributes['newcomer_redirect_uri'];
                     $redirect = $this->get_redirect_uri_with_user_meta_query_params($redirect, $user_attr_query_params);
                     echo "<script>window.location = '" . $redirect . "';</script>";
                 }
             } else if ($customer_status == "") {
+                error_log('C');
                 // set customer status to newcomer
                 update_user_meta(user_id: get_current_user_id(), meta_key: 'customer_status', meta_value: 'newcomer: Newcomer');
                 if (!empty($attributes['newcomer_redirect_uri'])) {
+                    error_log('C1');
                     $redirect = $attributes['newcomer_redirect_uri'];
                     $redirect = $this->get_redirect_uri_with_user_meta_query_params($redirect, $user_attr_query_params);
                     echo "<script>window.location = '" . $redirect . "';</script>";
+                } else {
+                    error_log('C2');
                 }
             }
 
