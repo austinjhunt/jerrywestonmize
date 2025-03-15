043fc38923f7cf7e541bb885ae0724b6b86acbad
Austin Hunt <austinsketches@gmail.com>
2025-03-15 17:47:56 -0400

    delete pagespeed ninja code

--- wp-content/advanced-cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* PageSpeed Ninja Caching */
-defined('ABSPATH') || die();
-if (is_file('/var/www/jerrywestonmize/wp-content/uploads/psn-pagespeed-ninja/advanced-cache-defines.php')) {
-  include_once '/var/www/jerrywestonmize/wp-content/uploads/psn-pagespeed-ninja/advanced-cache-defines.php';
-  include '/var/www/jerrywestonmize/wp-content/plugins/psn-pagespeed-ninja/public/advanced-cache.php';
-}
