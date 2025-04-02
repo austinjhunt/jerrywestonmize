acd57b7b74ec690b3c4bec84911768d30aab4215
Austin Hunt <austinsketches@gmail.com>
2025-04-02 13:40:47 -0400

    reapply stripe receipt email mod after amelia 8.1.1 update

--- wp-content/plugins/ameliabooking/src/Infrastructure/Services/Payment/StripeService.php
+++ wp-content/plugins/ameliabooking/src/Infrastructure/Services/Payment/StripeService.php
@@ -126,16 +126,28 @@ class StripeService extends AbstractPaymentService implements PaymentServiceInte
             if ($data['metaData']) {
                 $stripeData['metadata'] = $data['metaData'];
             }
+ 
 
+            // BEGIN MODS
+
+            if ($data['metaData']['Customer Email']) {
+                $stripeData['receipt_email'] = $data['metaData']['Customer Email'];
+            }
+            // also added a fallback description since that was appearing as null on the Stripe side
             if ($data['description']) {
                 $stripeData['description'] = $data['description'];
+            } else {
+                $stripeData['description'] = 'Payment for ' . $data['metaData']['Customer Name'] . ' - ' . $data['metaData']['Customer Email'] . ' - ' . $data['metaData']['Service'] . '';
             }
+            // END MODS
 
             $customerId = $this->createCustomer($data, $additionalStripeData);
 
             if ($customerId) {
                 $stripeData = array_merge($stripeData, ['customer' => $customerId]);
             }
+
+            
 
             $stripeData = apply_filters(
                 'amelia_before_stripe_payment',
